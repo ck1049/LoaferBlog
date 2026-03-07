bb2928b3c52c43d3e3d9c29c6747a4e7866b32eb
ck1049 <76576708+ck1049@users.noreply.github.com>
2026-03-07 23:31:01 +0800

    fix

--- backend/src/main/java/com/loafer/blog/model/entity/SensitiveWord.java
+++ backend/src/main/java/com/loafer/blog/model/entity/SensitiveWord.java
@@ -1,6 +1,7 @@
 package com.loafer.blog.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -13,7 +14,9 @@ public class SensitiveWord {
     @TableId(type = IdType.AUTO)
     private Long id;
     private String word;
+    @TableField("created_at")
     private LocalDateTime createTime;
+    @TableField("updated_at")
     private LocalDateTime updateTime;
     private Integer status;
 }
